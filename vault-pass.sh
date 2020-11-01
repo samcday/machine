2e7ad2e1cb9353291611f675716d77a21af08acf
Sam Day <me@samcday.com>
2020-11-01 12:32:44 +0100

    soup-up vault-pass.sh a little

--- vault-pass.sh
+++ vault-pass.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-secret-tool lookup ansible-vault samcday/machine
+password=$(secret-tool lookup ansible-vault samcday/machine)
+
+if [[ -z "${password}" ]]; then
+	password=$(zenity --password)
+	if [[ -z "${password}" ]]; then
+		exit 1
+	fi
+	echo -n $password | secret-tool store --label='ansible-vault/samcday/machine' ansible-vault samcday/machine
+fi
+
+echo $password
